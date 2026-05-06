b87b990e1b65ad0102592bdf81991f1342eb0204
DarilTom <dariltom775@gmail.com>
2026-05-06 20:59:44 +0530

    Add Render deployment config

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py migrate
+
+# Create the superuser if it doesn't exist
+python manage.py shell -c "
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+if not User.objects.filter(username='daril').exists():
+    User.objects.create_superuser('daril', 'admin@example.com', 'dtom3975')
+"
